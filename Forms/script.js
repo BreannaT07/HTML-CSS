cb727fffb750eeff6930bbcd8b90c246b55ea146
BreannaT07 <144352346+BreannaT07@users.noreply.github.com>
2025-03-20 14:09:50 -0400

    added changes

--- Forms/script.js
+++ Forms/script.js
@@ -1,5 +1,5 @@
 document.getElementById('feedbackForm').addEventListener
-('submit', function(e){e.preventDefault(); //Prevents the form fro submitting immediately
+('submit', function(e){e.preventDefault(); //Prevents the form from submitting immediately
 
     const fullName = document.getElementById('fullName').value;
     const age = parseInt(document.getElementById('age').value);
@@ -16,10 +16,7 @@ document.getElementById('feedbackForm').addEventListener
         alert("Please fill out this section.")
     }
 
-    else 
-    {
-        alert("Form Submitted.")
-    }
+    else {}
 
     // Int Types
     if (age === '' || phoneNumber === '' || zipCode === '')
@@ -27,17 +24,36 @@ document.getElementById('feedbackForm').addEventListener
         alert("Please fill out this section.")
     }
 
-    age = x;
-    let x = 18;
+    else {}
 
-    if (x < 18)
+    // Age Requirements
+    Number.parseInt(age, 18)
+
+
+    if (age = NaN) {
+        alert("You need numbers.");
+      } else if (age < 5) {
+        alert("You need to be above the age of 18.");
+      }     else {}
+
+    // Phone Number Requirements
+    phoneNumber = Number(true);
+
+    if (Number(false));
     {
-        alert("You need to be above the age of 18.")
+        alert("You need to type in numbers.")
     }
 
-    else 
-    {
-        
-    }
+    // Zip Code Requirements
+    Number.parseInt(zipCode, 5)
+    
+    if (zipCode = NaN) {
+        alert("You need numbers.");
+      } else if (zipCode < 5) {
+        alert("You need 5 numbers.");
+      } else if (zipCode > 5) {
+        alert("You need 5 numbers.");
+      }     else {}
+
 
 });
