103ed880f150b93b62516e1dc8f7c29d09cd5edb
BreannaT07 <144352346+BreannaT07@users.noreply.github.com>
2025-03-24 11:37:50 -0400

    added changes

--- Forms/script.js
+++ Forms/script.js
@@ -24,36 +24,41 @@ document.getElementById('feedbackForm').addEventListener
         alert("Please fill out this section.")
     }
 
-    else {}
+    else {
+    }
 
     // Age Requirements
-    Number.parseInt(age, 18)
+    
 
-
-    if (age = NaN) {
+    $number = min(max(Intval($number), 18), 99)
+    if (age = Number.NaN) {
         alert("You need numbers.");
-      } else if (age < 5) {
+      } else if (age < 18) {
         alert("You need to be above the age of 18.");
-      }     else {}
+      }     else {
+        alert("Form Submitted")
+      }
 
     // Phone Number Requirements
     phoneNumber = Number(true);
 
-    if (Number(false));
-    {
+      if (Number(false)) {
         alert("You need to type in numbers.")
-    }
+      } else {
+        alert("Form Submitted")   
+      }
 
     // Zip Code Requirements
     Number.parseInt(zipCode, 5)
-    
-    if (zipCode = NaN) {
+    if (zipCode = Number.NaN) {
         alert("You need numbers.");
       } else if (zipCode < 5) {
         alert("You need 5 numbers.");
       } else if (zipCode > 5) {
         alert("You need 5 numbers.");
-      }     else {}
+      }     else {
+        alert("Form Submitted")
+      }
 
 
 });
